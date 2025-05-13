85f253845e92a477d299a1a4011f035b1b9e9b33
luca-jt <random.lulu@die-genossen.de>
2025-05-13 23:49:43 +0200

    master password

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use copypasta::{ClipboardContext, ClipboardProvider};
 use orion::aead::{self, SecretKey};
+use orion::pwhash::{self, PasswordHash};
 use serde::{Deserialize, Serialize};
 use std::fs::{self, File};
 use std::io::{Read, Write, stdin, stdout};
 use std::path::PathBuf;
 
-const PASSKEY: &[u8] = b"lajgnrkeksnckvdpsymvki1ha67g0aa2"; // set that to a random value of 32 bytes
-const FILE: &str = "passwords.json";
+const STORAGE_FILE: &str = "passwords.json";
+const PASSWORD_HASH_FILE: &str = "used.pwhash";
+const ITERATIONS: u32 = 3;
+const MEMORY: u32 = 1 << 16;
 
 macro_rules! flushed_print {
     ($($arg:tt)*) => {
@@ -20,14 +23,12 @@ fn read_input_popped(buffer: &mut String) {
     buffer.pop();
 }
 
-fn encode(s: &str) -> Vec<u8> {
-    let secret = SecretKey::from_slice(PASSKEY).unwrap();
-    aead::seal(&secret, s.as_bytes()).unwrap()
+fn encode(s: &str, secret_key: &SecretKey) -> Vec<u8> {
+    aead::seal(secret_key, s.as_bytes()).unwrap()
 }
 
-fn decode(s: &[u8]) -> String {
-    let secret = SecretKey::from_slice(PASSKEY).unwrap();
-    String::from_utf8(aead::open(&secret, s).unwrap()).unwrap()
+fn decode(s: &[u8], secret_key: &SecretKey) -> String {
+    String::from_utf8(aead::open(secret_key, s).unwrap()).unwrap()
 }
 
 #[derive(Serialize, Deserialize, Debug, Default)]
@@ -38,28 +39,31 @@ struct Entry {
     password: Vec<u8>,
 }
 
-fn print_entry(entry: &Entry) {
+fn print_entry(entry: &Entry, secret_key: &SecretKey) {
     flushed_print!("PLATFORM:\t {}\n", entry.platform);
-    flushed_print!("USER NAME:\t {}\n", decode(&entry.user_name));
-    flushed_print!("EMAIL:\t\t {}\n", decode(&entry.email));
-    flushed_print!("PASSWORD:\t {}\n", decode(&entry.password));
+    flushed_print!("USER NAME:\t {}\n", decode(&entry.user_name, secret_key));
+    flushed_print!("EMAIL:\t\t {}\n", decode(&entry.email, secret_key));
+    flushed_print!("PASSWORD:\t {}\n", decode(&entry.password, secret_key));
 }
 
 struct Manager {
     clipboard: ClipboardContext,
     entries: Vec<Entry>,
-    file_path: PathBuf,
+    storage_file_path: PathBuf,
+    hash_file_path: PathBuf,
+    secret_key: SecretKey,
+    password_hash: PasswordHash,
 }
 
 impl Manager {
     fn new() -> Self {
-        let mut file_path = std::env::current_exe().unwrap();
-        file_path.set_file_name(FILE);
+        let mut storage_file_path = std::env::current_exe().unwrap();
+        storage_file_path.set_file_name(STORAGE_FILE);
 
-        let mut file = if !fs::exists(&file_path).unwrap() {
-            File::create_new(&file_path).unwrap()
+        let mut file = if !fs::exists(&storage_file_path).unwrap() {
+            File::create_new(&storage_file_path).unwrap()
         } else {
-            File::open(&file_path).unwrap()
+            File::open(&storage_file_path).unwrap()
         };
 
         let mut contents = String::new();
@@ -71,10 +75,61 @@ impl Manager {
             serde_json::from_str(&contents).unwrap()
         };
 
+        let mut hash_file_path = std::env::current_exe().unwrap();
+        hash_file_path.set_file_name(PASSWORD_HASH_FILE);
+        let mut password_string = String::new();
+
+        let password_hash = if !fs::exists(&hash_file_path).unwrap() {
+            loop {
+                flushed_print!("Choose a master password (8-32 chars): ");
+                read_input_popped(&mut password_string);
+                if password_string.len() >= 8 && password_string.len() <= 32 {
+                    while password_string.len() < 32 {
+                        password_string.push('0');
+                    }
+                    break;
+                }
+                flushed_print!("The password is not the correct lenght.\n");
+                password_string.clear();
+            }
+
+            let password = pwhash::Password::from_slice(password_string.as_bytes()).unwrap();
+            let password_hash = pwhash::hash_password(&password, ITERATIONS, MEMORY).unwrap();
+            let new_file_contents = serde_json::to_string(&password_hash).unwrap();
+            fs::write(&hash_file_path, new_file_contents.as_bytes()).unwrap();
+
+            password_hash
+        } else {
+            let hash_file_contents = fs::read(&hash_file_path).unwrap();
+            let password_hash = serde_json::from_slice(&hash_file_contents).unwrap();
+
+            loop {
+                flushed_print!("Enter your master password: ");
+                read_input_popped(&mut password_string);
+                while password_string.len() < 32 {
+                    password_string.push('0');
+                }
+                let password = pwhash::Password::from_slice(password_string.as_bytes()).unwrap();
+                let is_correct_pw = pwhash::hash_password_verify(&password_hash, &password).is_ok();
+
+                if is_correct_pw {
+                    break password_hash;
+                }
+
+                flushed_print!("Entered password is not correct.\n");
+                password_string.clear();
+            }
+        };
+
+        let secret_key = SecretKey::from_slice(password_string.as_bytes()).unwrap();
+
         Self {
             clipboard: ClipboardContext::new().unwrap(),
             entries,
-            file_path,
+            storage_file_path,
+            hash_file_path,
+            secret_key,
+            password_hash,
         }
     }
 
@@ -99,20 +154,21 @@ impl Manager {
 
         flushed_print!("Enter user name: ");
         read_input_popped(&mut buffer);
-        new_entry.user_name = encode(buffer.as_str());
+        new_entry.user_name = encode(buffer.as_str(), &self.secret_key);
         buffer.clear();
 
         flushed_print!("Enter email: ");
         read_input_popped(&mut buffer);
-        new_entry.email = encode(buffer.as_str());
+        new_entry.email = encode(buffer.as_str(), &self.secret_key);
         buffer.clear();
 
         flushed_print!("Enter password: ");
         read_input_popped(&mut buffer);
-        new_entry.password = encode(buffer.as_str());
+        new_entry.password = encode(buffer.as_str(), &self.secret_key);
         buffer.clear();
 
         self.entries.push(new_entry);
+        flushed_print!("Success!\n");
     }
 
     fn delete_entry(&mut self) {
@@ -127,7 +183,7 @@ impl Manager {
             let keep = entry.platform != buffer;
             if !keep {
                 flushed_print!("Deleted account information:\n\n");
-                print_entry(entry);
+                print_entry(entry, &self.secret_key);
                 found_entry = true;
             }
             keep
@@ -147,10 +203,10 @@ impl Manager {
 
         for entry in self.entries.iter() {
             if entry.platform == buffer {
-                print_entry(entry);
+                print_entry(entry, &self.secret_key);
 
                 self.clipboard
-                    .set_contents(decode(&entry.password))
+                    .set_contents(decode(&entry.password, &self.secret_key))
                     .unwrap();
 
                 flushed_print!("\nCopied password to clipboard.\n\n");
@@ -169,23 +225,58 @@ impl Manager {
         flushed_print!("\n");
         flushed_print!("---------------------------------------------------\n");
         for entry in self.entries.iter() {
-            print_entry(entry);
+            print_entry(entry, &self.secret_key);
             flushed_print!("---------------------------------------------------\n");
         }
         flushed_print!("\n");
+    }
+
+    fn change_password(&mut self) {
+        let mut new_password_string = String::new();
+
+        loop {
+            flushed_print!("Enter the new master password (8-32 chars): ");
+            read_input_popped(&mut new_password_string);
+            if new_password_string.len() >= 8 && new_password_string.len() <= 32 {
+                while new_password_string.len() < 32 {
+                    new_password_string.push('0');
+                }
+                break;
+            }
+            flushed_print!("The password is not the correct lenght.\n");
+            new_password_string.clear();
+        }
+
+        let password = pwhash::Password::from_slice(new_password_string.as_bytes()).unwrap();
+        let password_hash = pwhash::hash_password(&password, ITERATIONS, MEMORY).unwrap();
+        let new_file_contents = serde_json::to_string(&password_hash).unwrap();
+        fs::write(&self.hash_file_path, new_file_contents.as_bytes()).unwrap();
+        let new_secret_key = SecretKey::from_slice(new_password_string.as_bytes()).unwrap();
+
+        for entry in self.entries.iter_mut() {
+            entry.user_name = encode(&decode(&entry.user_name, &self.secret_key), &new_secret_key);
+            entry.email = encode(&decode(&entry.email, &self.secret_key), &new_secret_key);
+            entry.password = encode(&decode(&entry.password, &self.secret_key), &new_secret_key);
+        }
+
+        self.password_hash = password_hash;
+        self.secret_key = new_secret_key;
+        flushed_print!("Success!\n");
     }
 }
 
 impl Drop for Manager {
     fn drop(&mut self) {
         if self.entries.is_empty() {
-            if let Err(e) = fs::remove_file(&self.file_path) {
+            if let Err(e) = fs::remove_file(&self.storage_file_path) {
                 dbg!(e);
             }
             return;
         }
         let new_file_contents = serde_json::to_string(&self.entries).unwrap();
-        fs::write(&self.file_path, new_file_contents.as_bytes()).unwrap();
+        fs::write(&self.storage_file_path, new_file_contents.as_bytes()).unwrap();
+        let new_file_contents = serde_json::to_string(&self.password_hash).unwrap();
+        fs::write(&self.hash_file_path, new_file_contents.as_bytes()).unwrap();
     }
 }
 
@@ -194,7 +285,7 @@ fn main() {
     let mut command = String::new();
 
     loop {
-        flushed_print!("Enter a command. [new, del, get, view, close]: ");
+        flushed_print!("Enter a command. [new, del, get, view, close, change_password]: ");
         read_input_popped(&mut command);
 
         match command.as_str() {
@@ -202,6 +293,7 @@ fn main() {
             "del" => manager.delete_entry(),
             "get" => manager.get_entry(),
             "view" => manager.view_all(),
+            "change_password" => manager.change_password(),
             "close" => {
                 break;
             }
